ecade9548c74fd1a1b130b095809646930a4a536
CLEPIL <74221837+CLEPIL@users.noreply.github.com>
2022-10-25 16:42:44 +0900

    2nd

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,89 @@
 <template>
   <v-app>
     <div class="tittle">
-      <v-col>
-        注文内容
-      </v-col>
+      <v-col> 注文内容 </v-col>
     </div>
-    <div class="form">
+    <v-form>
       <v-row>
-        <v-col>
-          1
-        </v-col>
-        <v-col>
-          2
-        </v-col>
-        <v-col>
-          3
-        </v-col>
-        <v-col>
-          4
-        </v-col>
+        <v-col> 野菜 </v-col>
+        <v-col> 重量 </v-col>
+        <v-col> 受取日 </v-col>
+        <v-col> 見積もり </v-col>
       </v-row>
       <v-row>
         <v-col>
-          1
+          <v-select :items="items"> </v-select>
         </v-col>
         <v-col>
-          <v-select :items="items">
-          </v-select>
+          <v-text-field></v-text-field>
         </v-col>
         <v-col>
-          3
+          <v-menu v-model="menu">
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="text"
+                v-bind="attrs"
+                v-on="on"
+                label="ここから選択"
+                readonly
+                clearable
+              >
+              </v-text-field>
+            </template>
+            <v-date-picker v-model="picker" @input="formatDate(picker)">
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-col> {{price}}円 </v-col>
+      </v-row>
+      <v-row class="button">
+        <v-col>
+          <v-btn @click="addForm()">フォーム追加</v-btn>
         </v-col>
         <v-col>
-          4
+          <v-btn>送信</v-btn>
         </v-col>
       </v-row>
-    </div>
+    </v-form>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return{
-        items:['1','2'],
-      }
-    }
-  }
+export default {
+  data() {
+    return {
+      items: ["野菜1", "野菜2", "野菜3"],
+      g: ["250", "500", "750", "1000"],
+      menu: "",
+      text: "",
+      picker: "",
+      price: "200",
+    };
+  },
+  methods: {
+    formatDate(date) {
+      if (!date) return null;
+      const [year, month, day] = date.split("-");
+      this.text = `${year}${month}${day}`;
+      this.menu = false;
+      return;
+    },
+    addForm () {
+      this.forms.push('')
+    },
+  },
+};
 </script>
 
 <style>
-.tittle{
+.tittle {
   color: #000000;
   text-align: center;
 }
-.form{
+.form {
+  text-align: center;
+}
+.button{
   text-align: center;
 }
 </style>
